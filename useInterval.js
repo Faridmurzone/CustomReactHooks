702580239bb630e322da9ff262858705445f2fae
T0wn3R <faridmurzone@gmail.com>
2020-02-10 09:16:53 -0300

    Create useInterval.js
    useInterval proposed by Dan Abramov in https://overreacted.io/making-setinterval-declarative-with-react-hooks/ to make a good control of what, when and how interval works in our useEffect.

---
+++ useInterval.js
@@ -0,0 +1,16 @@
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+
+    let id = setInterval(tick, delay);
+    return () => clearInterval(id);
+  }, [delay]);
+}
